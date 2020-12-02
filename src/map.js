cf138b8837b93b3e78b8d55b582b156a8989a737
Nicolas Sartelet <nicolas.sartelet.mmi@gmail.com>
2020-12-02 04:13:36 +0100

    Ajout de la geolocalisation
    Ajout de la geolocalisation de l'utilisateur avec JS et ajout d'un marqueur sur la carte à son emplacement + ajout du fichier JSON avec les restaurants

--- src/map.js
+++ src/map.js
@@ -1,38 +1,66 @@
-$(function() {
-document.querySelector('.zoneMap').innerHTML = 'Affichage de la map google ici';
+$(function () {
+    $('.zoneMap').html('Affichage de la map google ici');
+
+    // Initialisation lat/long
+    let lat;
+    let lon;
+    let map = null;
+
+    /*----------------------------------------------------------------------
+    --------------|| Fonction d'initialisation de la carte ||---------------
+    ----------------------------------------------------------------------*/
+    function initMap() {
+        // ---- Création de l'objet map et insertion dans le html ----
+        map = new google.maps.Map(document.querySelector('.zoneMap'), {
+            // Centrage sur les coordonnées ci-dessus
+            center: new google.maps.LatLng(lat, lon),
+            // Définition du zoom par défaut
+            zoom: 14,
+            // Type de carte (ici routière)
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            // Activation des options de contrôle (plan, satellite..)
+            mapTypeControl: true,
+            mapTypeControlOptions: {
+                // type de placement des options
+                style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
+            },
+            // Activation des options de navigation (zoom...)
+            navigationControl: true,
+            navigationControlOptions: {
+                // Type d'affichage des options
+                style: google.maps.NavigationControlStyle.ZOOM_PAN
+            }
+        });
+
+        // ---- Ajout du marqueur ----
+        var marker = new google.maps.Marker({
+            // Position en syntaxe json
+            position: { lat: lat, lng: lon },
+            // Carte de destination
+            map: map
+        });
+    }
+
+    window.onload = function () {
+        // La fonction s'exécute lorsque le DOM est chargé
+        initMap();
+    };
+
+    /*----------------------------------------------------------------------
+    -------------------------|| GEOLOCALISATION ||--------------------------
+    ----------------------------------------------------------------------*/
+
+    navigator.geolocation.getCurrentPosition(success, error);
+
+    function success(position) {
+        //console.log(position.coords.latitude, position.coords.longitude);
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+    }
+
+    function error() {
+        console.log('Utilisateur non localisé');
+    }
+    
+
 });
-
-// On initialise la latitude et la longitude de Paris (centre de la carte)
-let lat = 48.852969;
-let lon = 2.349903;
-let map = null;
-
-// Fonction d'initialisation de la carte
-function initMap() {
-    // Créer l'objet "map" et l'insere dans l'html'
-    map = new google.maps.Map(document.querySelector('.zoneMap'), {
-        // Nous plaçons le centre de la carte avec les coordonnées ci-dessus
-        center: new google.maps.LatLng(lat, lon), 
-        // Nous définissons le zoom par défaut
-        zoom: 11, 
-        // Nous définissons le type de carte (ici carte routière)
-        mapTypeId: google.maps.MapTypeId.ROADMAP, 
-        // Nous activons les options de contrôle de la carte (plan, satellite...)
-        mapTypeControl: true, 
-        mapTypeControlOptions: {
-            // Cette option sert à définir comment les options se placent
-            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR 
-        },
-        // Activation des options de navigation dans la carte (zoom...)
-        navigationControl: true, 
-        navigationControlOptions: {
-            // Comment ces options doivent-elles s'afficher
-            style: google.maps.NavigationControlStyle.ZOOM_PAN 
-        }
-    });
-}
-window.onload = function(){
-    // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
-    initMap();
-    console.log('juste un test');
-};

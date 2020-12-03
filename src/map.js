bd190fe9491a06a44eb75d1c5f45e455807f7734
Nicolas Sartelet <nicolas.sartelet.mmi@gmail.com>
2020-12-03 14:05:57 +0100

    Ajout de la récupération des resto
    Restaurants récupérés et affichés dans la liste à côté de la map

--- src/map.js
+++ src/map.js
@@ -1,14 +1,21 @@
 $(function () {
     $('.zoneMap').html('Affichage de la map google ici');
 
-    // Initialisation lat/long
-    let lat;
-    let lon;
-    let map = null;
+    // ---- Appel des fonctions ----
+    window.onload = function () {
+        initMap();
+        getResto();
+    };
 
     /*----------------------------------------------------------------------
     --------------|| Fonction d'initialisation de la carte ||---------------
     ----------------------------------------------------------------------*/
+
+    // Init latitude/longitude
+    let lat;
+    let lon;
+    let map = null;
+
     function initMap() {
         // ---- Création de l'objet map et insertion dans le html ----
         map = new google.maps.Map(document.querySelector('.zoneMap'), {
@@ -37,14 +44,10 @@ $(function () {
             // Position en syntaxe json
             position: { lat: lat, lng: lon },
             // Carte de destination
-            map: map
+            map: map,
+            icon: 'media/icon_marker.png'
         });
     }
-
-    window.onload = function () {
-        // La fonction s'exécute lorsque le DOM est chargé
-        initMap();
-    };
 
     /*----------------------------------------------------------------------
     -------------------------|| GEOLOCALISATION ||--------------------------
@@ -59,8 +62,50 @@ $(function () {
     }
 
     function error() {
-        console.log('Utilisateur non localisé');
+        console.log('Utilisateur non localisé, service indisponible.');
     }
-    
+
+    /*----------------------------------------------------------------------
+    ------------------|| Récupération des resto JSON ||---------------------
+    ----------------------------------------------------------------------*/
+
+    function getResto(){
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                var response = JSON.parse(this.responseText);
+                let restaurants = response.restaurants;
+                console.table(restaurants);
+
+                // ---- Récupération de la position de chaque resto
+                for(let i = 0 ; i < restaurants.length ; i++){
+
+                    // ---- Ajout des marqueurs sur la map ----
+                    new google.maps.Marker({
+                        position: { lat: restaurants[i].lat, lng: restaurants[i].lon },
+                        map: map
+                    });
+                    
+                    // ---- Calcul de la moyenne des note de chaque resto ----
+                    let moyenneNote;
+                    function calculNote() {
+                        let somme = restaurants[i].ratings[0].stars + restaurants[i].ratings[1].stars ;
+                        moyenneNote = somme/restaurants[i].ratings.length;
+                        //console.log(moyenneNote);
+                    }
+                    calculNote();
+
+                    // ---- Ajout dans la liste ----
+                    $('.zoneListe li:eq(' + i + ')').html('<h4>' + restaurants[i].restaurantName + '</h4>' 
+                    + '<p class="restoAdress"> Adresse : ' + restaurants[i].address + '</p>'
+                    + '<p class="restoNote"> Note : '  + moyenneNote + '/5 </p>'
+                    + '<p class="restoComment"> Commentaire : '  + restaurants[i].ratings[0].comment + '</p>');
+                }                
+            }
+        };
+        request.open("GET","src/items.json");
+        request.send();
+    }
 
 });
+

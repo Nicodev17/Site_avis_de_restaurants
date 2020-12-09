ae46cfe92e105b565b03fd96f01f6754b45b4e24
Nicolas Sartelet <nicolas.sartelet.mmi@gmail.com>
2020-12-09 05:11:59 +0100

    Refactorisation totale du code
    Le code a été refactorisé, il y a maintenant des classes pour chaque élément (map et restaurants), le code est plus propre et l'api google maps est appelée d'une meilleure façon grâce à l'asynchrone et à une promise.
    
    On affiche donc la map seulement une fois que toutes les infos ont été reçues.
    
    Des méhodes d'instances de classes ont été créées pour être utilisée plus tard, par exemple pour la map, la méthode addMarker ou encore pour la class Restaurant la méthode addRating ou calculateAverage.

--- src/map.js
+++ src/map.js
@@ -1,111 +1,121 @@
-$(function () {
-    $('.zoneMap').html('Affichage de la map google ici');
+/*----------------------------------------------------------------------
+-------|| Initialisation map, position et restaurants JSON ||-----------
+----------------------------------------------------------------------*/
 
-    // ---- Appel des fonctions ----
-    window.onload = function () {
-        initMap();
-        getResto();
-    };
+// zone d'affichage de la map
+let zoneMap = document.querySelector('#zoneMap');
 
-    /*----------------------------------------------------------------------
-    --------------|| Fonction d'initialisation de la carte ||---------------
-    ----------------------------------------------------------------------*/
+class GoogleMap {
+    constructor() {
+        this.map = null;
+        this.userPosition = null;
+    }
 
-    // Init latitude/longitude
-    let lat;
-    let lon;
-    let map = null;
-
-    function initMap() {
-        // ---- Création de l'objet map et insertion dans le html ----
-        map = new google.maps.Map(document.querySelector('.zoneMap'), {
-            // Centrage sur les coordonnées ci-dessus
-            center: new google.maps.LatLng(lat, lon),
-            // Définition du zoom par défaut
-            zoom: 14,
-            // Type de carte (ici routière)
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            // Activation des options de contrôle (plan, satellite..)
-            mapTypeControl: true,
-            mapTypeControlOptions: {
-                // type de placement des options
-                style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
-            },
-            // Activation des options de navigation (zoom...)
-            navigationControl: true,
-            navigationControlOptions: {
-                // Type d'affichage des options
-                style: google.maps.NavigationControlStyle.ZOOM_PAN
-            }
+    /**
+     * Charge la carte sur un element
+     * @param {HTMLElement} element 
+     */
+    async load(element) {
+        return new Promise((resolve, reject) => {
+            // Récupération de la map via l'api google maps
+            $script('https://maps.googleapis.com/maps/api/js?key=AIzaSyAOC9ObG1y6HwJN-04mYSZy90W4nQOVs3k', () => {
+                let center = {lat: 48.859626, lng: 2.350331};   
+                this.map = new google.maps.Map(element, {
+                    zoom: 14,
+                    center: center,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                    mapTypeControl: true,
+                    mapTypeControlOptions: {
+                        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
+                        },
+                    navigationControl: true,
+                    navigationControlOptions: {
+                        style: google.maps.NavigationControlStyle.ZOOM_PAN
+                    }
+                });
+                resolve();
+            });
         });
+    } // fin fonction load
 
-        // ---- Ajout du marqueur ----
-        var marker = new google.maps.Marker({
-            // Position en syntaxe json
-            position: { lat: lat, lng: lon },
-            // Carte de destination
-            map: map,
+/*----------------------------------------------------------------------
+-------------------|| Fonction de geolocalisation ||--------------------
+----------------------------------------------------------------------*/
+    async geoloc() {
+        return new Promise((resolve, reject) => {
+            //Géolocalisation de l'utilisateur via l'api JS
+            navigator.geolocation.getCurrentPosition((pos)=> {
+                this.userPosition = {
+                    lat: pos.coords.latitude,
+                    lng: pos.coords.longitude
+                }
+                //Centrage de la map sur l'user
+                //mapClass.map.center = new google.maps.LatLng(mapClass.userPosition.lat, mapClass.userPosition.lng);
+                this.map.panTo(this.userPosition);
+                // Ajout du marker de l'user
+                let userMarker = new google.maps.Marker ({
+                    position: this.map.center,
+                    map: this.map,
+                    icon: 'media/person_icon.png'
+                });
+                resolve();
+            });
+        });
+    } // fin fonction geoloc
+    
+    /**
+     * Ajoute un marqueur sur la carte
+     * @param {string} lat 
+     * @param {string} lng 
+     */
+    addMarker(lat, lng) {
+        let marker = new google.maps.Marker ({
+            position: new google.maps.LatLng(lat, lng),
+            map: this.map,
             icon: 'media/icon_marker.png'
         });
     }
+} // fin class GoogleMap
 
-    /*----------------------------------------------------------------------
-    -------------------------|| GEOLOCALISATION ||--------------------------
-    ----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------
+--------------------|| Fonction d'initialisation ||---------------------
+----------------------------------------------------------------------*/
+const initMap = async function() {
+    let mapClass = new GoogleMap();
+    await mapClass.load(zoneMap);
+    await mapClass.geoloc();
 
-    navigator.geolocation.getCurrentPosition(success, error);
+    // Requête pour obtenir tous les resto du fichier JSON
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            let restaurants = response;
+            console.table(restaurants);
 
-    function success(position) {
-        //console.log(position.coords.latitude, position.coords.longitude);
-        lat = position.coords.latitude;
-        lon = position.coords.longitude;
-    }
+            // Pour chaque resto
+            for(let i = 0 ; i < restaurants.length ; i++) {
+                let item = restaurants[i];
 
-    function error() {
-        console.log('Utilisateur non localisé, service indisponible.');
-    }
+                // Création d'yn marqueur
+                mapClass.addMarker(item.lat, item.lon);
 
-    /*----------------------------------------------------------------------
-    ------------------|| Récupération des resto JSON ||---------------------
-    ----------------------------------------------------------------------*/
-
-    function getResto(){
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function() {
-            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-                var response = JSON.parse(this.responseText);
-                let restaurants = response.restaurants;
-                console.table(restaurants);
-
-                // ---- Récupération de la position de chaque resto
-                for(let i = 0 ; i < restaurants.length ; i++){
-
-                    // ---- Ajout des marqueurs sur la map ----
-                    new google.maps.Marker({
-                        position: { lat: restaurants[i].lat, lng: restaurants[i].lon },
-                        map: map
-                    });
-                    
-                    // ---- Calcul de la moyenne des note de chaque resto ----
-                    let moyenneNote;
-                    function calculNote() {
-                        let somme = restaurants[i].ratings[0].stars + restaurants[i].ratings[1].stars ;
-                        moyenneNote = somme/restaurants[i].ratings.length;
-                        //console.log(moyenneNote);
-                    }
-                    calculNote();
-
-                    // ---- Ajout dans la liste ----
-                    $('.zoneListe li:eq(' + i + ')').html('<h4>' + restaurants[i].restaurantName + '</h4>' 
-                    + '<p class="restoAdress"> Adresse : ' + restaurants[i].address + '</p>'
-                    + '<p class="restoNote"> Note : '  + moyenneNote + '/5 </p>'
-                    + '<p class="restoComment"> Commentaire : '  + restaurants[i].ratings[0].comment + '</p>');
-                }                
+                // Création d'un objet contenant les méthodes d'instance
+                const restaurant = new Restaurant(i, item.restaurantName, "media/favicon_burger.png", item.address, item.lat, item.lon, item.ratings);
+                console.log(restaurant);
+                
+                // Affichage dans la liste
+                $('.zoneListe li:eq(' + i + ')').html('<h4>' + item.restaurantName + '</h4>' 
+                + '<p class="restoAdress"> Adresse : ' + item.address + '</p>'
+                + '<p class="restoNote"> Note : '  + item.ratings[0].stars + '/5 </p>'
+                + '<p class="restoComment"> Commentaire : '  + item.ratings[0].comment + '</p>');
             }
-        };
-        request.open("GET","src/items.json");
-        request.send();
+        }
     }
+    request.open("GET","src/items.json");
+    request.send();
+}
 
-});
-
+if (zoneMap !== null) {
+    initMap();
+}

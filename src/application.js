7a01c6a9ecd455dbae55222bc6883fb99be055cf
Nicolas Sartelet <nicolas.sartelet.mmi@gmail.com>
2020-12-10 14:02:00 +0100

    Amélioration de la liste + ajout pop up

--- src/application.js
+++ src/application.js
@@ -11,11 +11,11 @@ $(function() {
       max: 5,
       values: [ 1, 5 ],
       slide: function( event, ui ) {
-        $( "#note" ).val( "Note entre " + ui.values[0] + " ★ et " + ui.values[1] + " ★" );
+        $( "#note" ).val( "Note entre " + ui.values[0] + " et " + ui.values[1] + " ★" );
       }
     });
     $( "#note" ).val( "Note entre " + $( "#slider-range" ).slider( "values", 0 ) +
-      " ★ et " + $( "#slider-range" ).slider( "values", 1 ) + " ★" );
+      " et " + $( "#slider-range" ).slider( "values", 1 ) + " ★" );
  
   } );
 
@@ -27,7 +27,8 @@ async function initResto() {
   let mapClass = new GoogleMap();
   await mapClass.load(zoneMap);
   await mapClass.geoloc();
-  //let marker = null;
+  let currentPopup = null;
+
 
   // Requête pour obtenir tous les resto du fichier JSON
   var request = new XMLHttpRequest();
@@ -40,11 +41,12 @@ async function initResto() {
       // Pour chaque resto
       for(let i=0 ; i < restaurants.length ; i++) {
         let item = restaurants[i];
-        let listItem = $('.zoneListe li:eq(' + i + ')');
+        let listItem = $('#zoneListe li:eq(' + i + ')');
 
         // Création d'un marqueur
         let marker = mapClass.addMarker(item.lat, item.lon, 'media/icon_marker.png');
 
+        // Evenement du comportement des marqueurs au survol d'un item
         $(document).ready(function event() {
           listItem.hover(
               function() {
@@ -54,7 +56,48 @@ async function initResto() {
                 marker.desactivated();
               }
           );
-      });
+        });
+
+        $(document).ready(function event() {
+          listItem.click(
+              function() {
+                listItem.css('display', 'block');
+              },
+          );
+        });
+
+        // Création de la bulle infoWindow pour le marqueur
+        // const popup = new google.maps.InfoWindow({
+        //   content: item.restaurantName,
+        //   maxWidth:300,
+        //   maxHeight:100
+        // });
+
+        // marker.addListener("click", () => {
+        //   console.log('test');
+        //   //popup.open(mapClass.map, marker);
+        // });
+
+        // google.maps.event.addListener(marker, "click", function() {
+        //   console.log('test');
+        //   // On ouvre la bulle correspondant à notre marqueur
+        //   popup.open(mapClass.map, marker);
+        //   // On enregistre cette bulle dans la variable currentPopup
+        //   currentPopup = popup;
+        //   // Si une bulle est déjà ouverte
+        //   if (currentPopup != null) {
+        //     // On la ferme
+        //     currentPopup.close();
+        //     // On vide la variable
+        //     currentPopup = null;
+        //   }
+        // });
+        // Nous activons l'écouteur d'évènement "closeclick" sur notre bulle
+        // pour surveiller le clic sur le bouton de fermeture
+        // google.maps.event.addListener(popup, "closeclick", function() {
+        //   // On vide la variable
+        //   currentPopup = null;
+        // });
 
         // Création d'un objet contenant les méthodes d'instance
         const restaurant = new Restaurant(item.restaurantName, "media/favicon_burger.png", item.address, item.lat, item.lon, item.ratings);
@@ -62,9 +105,8 @@ async function initResto() {
         
         // Affichage dans la liste
         listItem.html('<h4>' + item.restaurantName + '</h4>' 
-        + '<p class="restoAdress"> Adresse : ' + item.address + '</p>'
-        + '<p class="restoNote"> Note : '  + item.ratings[0].stars + '/5 </p>'
-        + '<p class="restoComment"> Commentaire : '  + item.ratings[0].comment + '</p>');
+        + '<p class="restoAdress">' + item.address + '</p>'
+        + '<p class="restoNote">'  + item.ratings[0].stars + '/5 ★ </p>').css('display', 'block');
       };
 
 
@@ -72,7 +114,5 @@ async function initResto() {
   }
 request.open("GET","src/items.json");
 request.send();
-
-
 
 } // Fin fonction initResto
